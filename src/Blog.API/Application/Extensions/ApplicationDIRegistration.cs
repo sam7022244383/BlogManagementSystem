f4f39cc522277eefb5a1c54d040671ed35cfe041
sam7022244383 <samimujawar7@gmail.com>
2024-10-14 16:26:14 +0530

    added auto mapper skeleton

--- src/Blog.API/Application/Extensions/ApplicationDIRegistration.cs
+++ src/Blog.API/Application/Extensions/ApplicationDIRegistration.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,12 @@ namespace Application.Extensions
 {
     public static class ApplicationDIRegistration
     {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(x => x.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            return services;
+        }
+
     }
 }
